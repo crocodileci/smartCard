1282a91a09a9ef62cc1180c6b8fa411111db23b4
Grady Dun <gradydun@hitrust.com.tw>
2019-07-22 17:15:48 +0800

    更新 BankInfo interface

--- src/app/model/CardInfo.ts
+++ src/app/model/CardInfo.ts
@@ -1,25 +1,36 @@
-export interface CardInfo{
-    issuer: BankInfo,
+export interface CardInfo {
+  issuer: BankInfo,
     mainAccount: string
 }
 
-export interface BankInfo{
-    value: string,
+export interface BankInfo {
+  value: string,
     label: string
 }
 
 export interface TransData {
-    transDate?: string;
-    transTime?: string;
-    transType?: string;
-    transSerial?: string;
-    issuerBank?: BankInfo;
-    issuerAccount?: string;
-    transBank?: BankInfo;
-    transAccount?: string;
-    amount?: string;
-    balance?: string;
-    returnCode?: string;
-    transResult?: string;
-    tac?: string;
+  transDate ? : string;
+  transTime ? : string;
+  transType ? : string;
+  transSerial ? : string;
+  issuerBank ? : BankInfo;
+  issuerAccount ? : string;
+  transBank ? : BankInfo;
+  transAccount ? : string;
+  amount ? : string;
+  balance ? : string;
+  returnCode ? : string;
+  transResult ? : string;
+  tac ? : string;
 }
+
+declare namespace hitrust {
+    namespace plugins{
+        export interface CardReader{
+            isReaderExisted(successCallback: (isReaderExisted: boolean) => void, errorCallback: (error: any) => void);
+        }
+    }
+}
+
+export declare var cardReader: hitrust.plugins.CardReader;
+
