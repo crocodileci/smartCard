dcee803a42934930681f619d4638aa1438b99ffa
Grady Dun <gradydun@hitrust.com.tw>
2019-07-24 15:36:29 +0800

    hand-shake-service.service.ts 新增 testNormalTelegram function用來測試加密通道與中台的通訊 新增 handshake function 用來處理與中台交換key的功能

--- src/app/services/handshake/hand-shake-service.service.ts
+++ src/app/services/handshake/hand-shake-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { from } from 'rxjs';
 
 declare var hitrust: any;
 
@@ -35,12 +36,68 @@ export class HandShakeServiceService {
     return this.service_communicate;
   }
 
+  public get responseServiceName(): string{
+    return this.service_response;
+  }
+
   public get serverURL(): string {
     return this.url;
   }
 
   public get currentClientSessionId(): string {
     return this.clientSessionId;
+  }
+
+  handshake(){
+    return from(new Promise((resolve, reject) => {
+      hitrust.plugins.e2ee.generateChallenge((challenge) => {
+        this.http.post<any>(this.url + this.service_challenge, challenge).subscribe(res => {
+          this.clientSessionId = res.clientSessionId;
+          let serverResponse = {
+            serverChallenge: res.serverChallenge,
+            publicKey: res.serverPublicKey,
+            serverResponse: res.serverResponse
+          }
+          hitrust.plugins.e2ee.verifyResponse(serverResponse, (clientResponse) => {
+
+            console.log(clientResponse);
+
+            clientResponse.clientSessionId = this.clientSessionId;
+
+            this.http.post<any>(this.url + this.service_response, clientResponse).subscribe(res => {
+              console.log(res);
+
+              if (res == "Challenge Response & synchonize session key complete.") {
+                console.log("Handshake done");
+                resolve(true);
+              }else{
+                resolve(false);
+              }
+            });
+
+          }, reject);
+
+        });
+      }, reject);
+
+    }));
+
+  }
+
+  testNormalTelegram(){
+    let telegram_101 = {
+      userId: "Grady",
+      caseId: "123",
+      actionId: "456",
+      company: "Histrust",
+      name: "GradyDun"
+    }
+
+    this.http.post<any>(this.url + this.service_communicate, telegram_101).subscribe(res => {
+
+      console.log(res);
+
+    });
   }
 
   testHandshake() {
@@ -67,15 +124,10 @@ export class HandShakeServiceService {
 
           clientResponse.clientSessionId = this.clientSessionId;
 
-          this.http.post(this.url + this.service_response, clientResponse, { observe: 'response', responseType: 'text' }).subscribe(res => {
+          this.http.post<any>(this.url + this.service_response, clientResponse).subscribe(res => {
             console.log(res);
-            let body = JSON.parse(res.body);
-            console.log(body);
 
-            hitrust.plugins.e2ee.sessionKeyDecrypt(body.answer, (result) => {
-              console.log(result);
-
-              if (result == "Challenge Response & synchonize session key complete.") {
+            if (res == "Challenge Response & synchonize session key complete.") {
                 console.log("Handshake done");
 
                 let telegram_101 = {
@@ -90,23 +142,8 @@ export class HandShakeServiceService {
 
                   console.log(res);
 
-                  let body = res;
-
-                  hitrust.plugins.e2ee.sessionKeyDecrypt(body.communication, (plainText) => {
-                    console.log(plainText);
-
-                    let body = JSON.parse(plainText);
-
-                    console.log(body);
-
-                  }, console.error);
-
                 });
-
-
               }
-
-            }, console.error);
           });
 
         }, console.error);
